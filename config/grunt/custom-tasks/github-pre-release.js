d225f267d48564e1b081467f1bfd3c141c1be6cb
IreneStr <irene@yoast.com>
2021-03-08 15:54:31 +0100

    Add beta creation commands

--- config/grunt/custom-tasks/github-pre-release.js
+++ config/grunt/custom-tasks/github-pre-release.js
@@ -42,9 +42,9 @@ module.exports = function( grunt ) {
 			const pluginVersion = grunt.file.readJSON( "package.json" ).yoast.pluginVersion;
 
 			let initialContent = `Changes compared to ${grunt.config.get( "previousPluginVersion" )}:\n`;
-			// For the first RC use the full changelog, for follow-up RCs only use the changes.
-			if ( pluginVersion.substr( pluginVersion.length - 3 ) === "RC1" ) {
-				initialContent = `Changes in this RC (${pluginVersion}):\n`;
+			// For the first beta use the full changelog. For follow-up betas and RCs, only use the changes.
+			if ( pluginVersion.substr( pluginVersion.length - 3 ) === "beta1" ) {
+				initialContent = `Changes in this beta (${pluginVersion}):\n`;
 			}
 
 			// Open a text editor to get the changelog.
